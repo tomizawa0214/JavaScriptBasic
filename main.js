5cca621c12ea259805e50f7b363ad21fefd28df5
tomizawa0214 <j.tomizawa.japan@gmail.com>
2020-02-05 09:39:20 +0900

    JSpractice①

---
+++ main.js
@@ -0,0 +1,12 @@
+'use strict';
+
+// 定数 const
+// 変数 let var
+
+let price = 150;
+console.log(price * 140);
+console.log(price * 160);
+
+price = 170;
+console.log(price * 140);
+console.log(price * 160);
