d89b4f97d4248f9aceb16b321fef344ffff89c41
tomizawa0214 <j.tomizawa.japan@gmail.com>
2020-02-08 19:43:54 +0900

    JSPractice③

--- main.js
+++ main.js
@@ -1,15 +1,6 @@
 'use strict';
 
-// let hp = 100;
-//
-// while (hp > 0) {
-//   console.log(`${hp} HP left!`)
-//   hp -= 15;
-// }
-
-let hp = -50;
-
-do {
-  console.log(`${hp} HP left!`)
-  hp -= 15;
-} while (hp > 0)
+{
+  const x = 100;
+  console.log(x);
+}
