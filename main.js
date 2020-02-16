7bb1ac25dd40c1b74ff494d553cf21f8937ce91b
tomizawa0214 <j.tomizawa.japan@gmail.com>
2020-02-16 19:07:16 +0900

    JSPractice⑤

--- main.js
+++ main.js
@@ -1,6 +1,12 @@
 'use strict';
 
 {
-  const x = 100;
-  console.log(x);
+  document.querySelector('button').addEventListener('click',() => {
+    const item0 = document.querySelectorAll('li')[0];
+    const copy = item0.cloneNode(true);
+
+    const ul = document.querySelector('ul');
+    const item2 = document.querySelectorAll('li')[2];
+    ul.insertBefore(copy, item2);
+  });
 }
